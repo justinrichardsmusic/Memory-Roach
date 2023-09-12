1125a214d8c8000d5399fc640a90331ef38515d4
Justin Richards <66257247+justinrichardsmusic@users.noreply.github.com>
2023-09-12 21:32:48 +1000

    Create Source.h

---
+++ Source.h
@@ -0,0 +1,17 @@
+#pragma once
+#define OLC_PGE_APPLICATION
+#include "olcPixelGameEngine.h"
+
+#define PGE_GAME_2D
+#define PGE_GAME_WITH_AUDIO
+#include "PGE_GAME_2D.h"
+#include "PGE_GAME_2D_Constants.h"
+
+#include "cGameCore.h"
+#include "cMenu.h"
+#include "cUnit.h"
+#include "cMemBlock.h"
+#include "cHud.h"
+#include "cUI.h"
+
+int main();
